380244878c3d0bb60172485815e27161e657a90b
Pedro Klepa <pedro.klepa@gmail.com>
2021-02-01 16:23:05 +0000

    Update favicon.ico and home title

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Cat Wiki - Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
