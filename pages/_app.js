ce727a4fd2e620eba0d53d7d383defdd1876662d
Pedro Klepa <pedro.klepa@gmail.com>
2021-02-08 14:47:57 +0000

    Install nprogress and implement it on _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from 'styled-components';
 
-import GlobalStyles from "../styles/globalStyles";
-import theme from "../styles/themes";
+import GlobalStyles from '../styles/globalStyles';
+import theme from '../styles/themes';
+
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+//Binding events.
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function App({ Component, pageProps }) {
   return (
