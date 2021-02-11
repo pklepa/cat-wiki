adba9b43ad59516d9962ebfac84b64d049986d13
Pedro Klepa <pedro.klepa@gmail.com>
2021-02-11 15:31:45 +0000

    Add a comment in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ import Router from 'next/router';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
 
-//Binding events.
+// Binding events for nprogress bar
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
