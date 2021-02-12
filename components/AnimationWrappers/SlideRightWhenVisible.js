427e61f1b098291abd97e840c29a40d297390dba
Pedro Klepa <pedro.klepa@gmail.com>
2021-02-12 14:24:33 +0000

    Lower  SlideRight threshhold for the same reason as above

--- components/AnimationWrappers/SlideRightWhenVisible.js
+++ components/AnimationWrappers/SlideRightWhenVisible.js
@@ -4,7 +4,7 @@ import { useInView } from 'react-intersection-observer';
 
 function SlideRightWhenVisible({ children }) {
   const controls = useAnimation();
-  const [ref, inView] = useInView({ threshold: 0.5 });
+  const [ref, inView] = useInView({ threshold: 0.4 });
 
   useEffect(() => {
     if (inView) {
